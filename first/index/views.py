16736f931d91f419a4f6d642672c1c3857d5994c
unni <sri654jith@gmail.com>
2021-07-14 01:06:14 +0530

    dbserver

--- first/index/views.py
+++ first/index/views.py
@@ -2,23 +2,27 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
 from django.urls import reverse
-
-users = ["srijith"]
+from .models import users
 
 class userform(forms.Form):
     name = forms.CharField(label="New User")
+    motto = forms.CharField(label="Motto")
 
 # Create your views here.
 def index(response):
+    x = users.objects.all()
     return render(response, "index/index.html",{
-        "users" : users
+        "users" : x
     })
 
 def add(response):
     if response.method == "POST":
         form = userform(response.POST)
         if form.is_valid():
-            users.append(form.cleaned_data["name"])
+            name=form.cleaned_data["name"]
+            motto=form.cleaned_data["motto"]
+            q = users(user_name = name, motto = motto)
+            q.save()
             return HttpResponseRedirect(reverse("ind:index"))
 
     return render(response, "index/add.html",{
