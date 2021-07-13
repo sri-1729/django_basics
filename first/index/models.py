8c17faf8a2d411265d06e6f7a22fdf2ccaeb3ea8
unni <sri654jith@gmail.com>
2021-07-13 14:00:43 +0530

    added database

--- first/index/models.py
+++ first/index/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class users(models.Model):
+    user_name = models.CharField(max_length=100)
+    motto = models.CharField(max_length=500)
+    def __str__(self):
+        return self.user_name
+
